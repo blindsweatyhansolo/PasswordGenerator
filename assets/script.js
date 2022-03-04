0b96093643c4e10f8e6b7efacfb13ea855aa0c11
J Semidey <mtsemidey@gmail.com>
2022-03-04 13:10:12 -0500

    set variables for characters, password length and build string functions

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,31 @@
+/** Criteria
+ * password length (prompt, 8-128 characters)
+ * special characters (confirm)
+ * integers (confirm, validate if NaN)
+ * uppercase (confirm)
+ * lowercase (confirm)
+ */
 // Assignment code here
+
+var specialChar;
+var numbers;
+var upperCaseChar;
+var lowerCaseChar;
+
+var passLength = function() {
+    var passwordLength = prompt("How many characters would you like your password to be?");
+
+    if (isNaN(passwordLength)) {
+        alert("Password length must be an integer (number)");
+        passLength();
+    } else if (passwordLength > 128 || passwordLength < 8) {
+        alert("Password length must be between 8 and 128 characters.");
+    } else {
+        return passwordLength;
+    }
+}
+
+console.log(passLength);
 
 
 // Get references to the #generate element
